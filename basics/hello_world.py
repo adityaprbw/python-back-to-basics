769e13067b0fd356ca966ee4cfa4276fceae7982
= <=>
2025-06-12 14:01:05 +0700

    Hello world

---
+++ basics/hello_world.py
@@ -0,0 +1,15 @@
+print("Hello world!")
+
+"""
+Aturan penulisan python:
+
+- tidak direkomendasikan mengakhiri statement dgn semicolon
+- indentasi merupakan whitespace utk memberikan tanda bahwa baris ini merupakan bagian dari suatu code block. whitespace pada indentasi harus konsisten jumlahnya
+"""
+
+# contoh indentasi pada function
+def say_greetings_in_japanese():
+    print("Konnichiwa!")
+    print("Ohayou gozaimasu")
+
+say_greetings_in_japanese()
