ebce75bc6ae73d60e5a91dd7d3cda43baede1179
Aditya Prabowo <ask.adityaprabowo@gmail.com>
2025-06-13 17:44:46 +0700

    Add examples of arithmetic operators

---
+++ basics/belajar_operator_aritmatika.py
@@ -0,0 +1,43 @@
+x = 10
+y = 3
+
+# penjumlahan
+result = x + y
+print(result)
+print()
+
+# pengurangan
+result = x - y
+print(result)
+print()
+
+# pembagian
+result = x / y
+print(result)
+print()
+
+# perkalian
+result = x * y
+print(result)
+print()
+
+# eksponen/pangkat
+result = x ** y
+print(result)
+print()
+
+# floor division
+result = x // y
+print(result)
+print()
+
+# modulo
+result = x % y
+print(result)
+print()
+
+print("\n==========\n")
+
+# operator precedence
+x = 5 * (31 + 2) // 4 ** 2
+print(x)
